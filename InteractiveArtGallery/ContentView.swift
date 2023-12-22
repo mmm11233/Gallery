a0488ceabd205be78b8d562d96901be4d0da155c
Mariam Joglidze <mariamjoglidze1@gmail.com>
2023-12-22 12:42:40 +0400

    Initial Commit

---
+++ InteractiveArtGallery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractiveArtGallery
+//
+//  Created by Mariam Joglidze on 22.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveArtGallery/InteractiveArtGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveArtGalleryApp.swift
+//  InteractiveArtGallery
+//
+//  Created by Mariam Joglidze on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveArtGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
